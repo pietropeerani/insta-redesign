0867937aa5ce291e93575870eb0e49096fa1cfad
pietro <pietro.peerani@gmail.com>
2025-06-13 18:04:40 +0200

    black screen for unsupported screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import Header from "./components/header"
-import Navbar from "./components/navbar"
-import ProfileCover from "./components/profile-cover"
+import { useEffect, useState } from 'react';
+import Header from './components/header';
+import ProfileCover from './components/profile-cover';
+import Navbar from './components/navbar';
 
-function App() {
+export default function App() {
+  const [isSupportedScreen, setIsSupportedScreen] = useState(window.innerWidth <= 428 && window.innerHeight <= 926);
 
-  return (
-    <>
-     <Header />
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSupportedScreen(window.innerWidth <= 428 && window.innerHeight <= 926);
+    };
 
-     <ProfileCover />
+    window.addEventListener('resize', handleResize);
 
-     <div className="super-ellipse"></div>
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-      <div className="fixed top-0 left-0 -z-20">
-        <div className="absolute z-0 top-1/4 left-0 h-full w-full bg-[linear-gradient(#66D3C200,#00504E_37%)]"></div>
-        <img src="/cover.png" alt="" />
+  if (isSupportedScreen) {
+    return (
+      <>
+        <Header />
+        <ProfileCover />
+        <div className="super-ellipse"></div>
+        <div className="fixed top-0 left-0 -z-20">
+          <div className="absolute z-0 top-1/4 left-0 h-full w-full bg-[linear-gradient(#66D3C200,#00504E_37%)]"></div>
+          <img src="/cover.png" alt="" />
+        </div>
+        <Navbar />
+      </>
+    );
+  } else {
+    return (
+      <div className="flex justify-center items-center bg-black h-screen">
+        <h1 className="!text-9xl font-bold text-white" style={{ fontFamily: 'Instagram Sans' }}>
+          Screen unsupported
+        </h1>
       </div>
-
-      <Navbar />
-    </>
-  )
+    );
+  }
 }
-
-export default App
